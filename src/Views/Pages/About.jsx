d181c50c1a539e566ee700e213a6e6492ae6ed3d
KevinC37 <49222836+KevinC37@users.noreply.github.com>
2021-12-16 13:33:51 +0200

    filled contactus and about pages

--- src/Views/Pages/About.jsx
+++ src/Views/Pages/About.jsx
@@ -1,12 +1,59 @@
-import React from "react"
-import "../Styles/body.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Styles/body.css';
+import '../Styles/Pages/about.css';
 
 function AboutPage() {
-  return(
+  return (
     <div>
-      This is the about page.
+      <div className="hero___container">
+        <div className="hero___wrapper">
+          <img
+            src="https://cdn.searchenginejournal.com/wp-content/uploads/2019/01/Top-10-Ranking-About-Us-Pages.png"
+            alt=""
+            className="hero___background___image"
+          />
+          <h1 className="hero___title">
+            A Place for People <br />
+            and Their Ideas
+          </h1>
+
+          <h3 className="hero___subtitle">
+            Media Group strives to provide an elevated employee experience with
+            a wide range of benefits. On top of excellent health and wellbeing
+            benefits, we are always working to offer innovative programs and
+            perks across our global workforce.
+          </h3>
+
+          <hr className="hero___divider" />
+        </div>
+      </div>
+
+      <div className="core___container">
+        <div className="core___wrapper">
+          <h3 className="core___title">Diversity, Equity and Inclusion</h3>
+
+          <h3 className="core___subtitle">Core Behaviors</h3>
+          <h4>
+            How we create is intrinsically linked to the values we uphold. These
+            core behaviors influence everything we do. Consider this a guide to
+            how we treat ourselves, and each other, as we create work that
+            pushes the world forward for our audience.
+          </h4>
+        </div>
+        <hr className="core___divider" />
+      </div>
+
+      <div className="connect___conainer">
+        <div className="connect___wrapper">
+          <h1>Let's Connect</h1>
+          <Link to="/contact">
+            <h3 className="contact___us">Contact us</h3>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AboutPage;
--- src/Views/Pages/Contactus.jsx
+++ src/Views/Pages/Contactus.jsx
@@ -1,13 +1,87 @@
-import React from "react";
-import "../Styles/body.css";
+import React from 'react';
+import '../Styles/body.css';
+
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+/* React forms imports */
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+/* Material UI Imports */
+import { TextField, Button } from '@material-ui/core';
+import { Snackbar } from '@material-ui/core';
+
+const EMAIL_TO = `m.cujba@yahoo.com`;
 
 function ContactPage() {
-  return (
-    <div>
-      This is the contact page.
-    </div>
-  )
+  const userName =
+    localStorage.getItem('firstName') + '' + localStorage.getItem('lastName');
 
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required('Please fill this field'),
+    body: Yup.string().required('Please fill this field'),
+  });
+
+  const formOptions = { resolver: yupResolver(validationSchema) };
+
+  const { register, handleSubmit, formState } = useForm(formOptions);
+  const { errors } = formState;
+  const redirect = useNavigate();
+  const [submitStatus, setSubmitStatus] = useState(false); //for triggering the 'Success' popup
+
+  function onSubmit(data) {
+    window.open(`mailto:${EMAIL_TO}?subject=${data.title}&body=${data.body}`);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="form___main">
+      <h2 className="form___header">Contact us</h2>
+      <h3 className="form___header">We are all ears</h3>
+
+      <TextField
+        type="title"
+        error={!!errors.title}
+        margin="normal"
+        variant="outlined"
+        label="Title"
+        multiline
+        minRows={1}
+        maxRows={2}
+        {...register('title')}
+      />
+      {errors.title && <p>{errors.title?.message}</p>}
+
+      <TextField
+        type="body"
+        error={!!errors.body}
+        margin="normal"
+        variant="outlined"
+        label="Body"
+        multiline
+        minRows={10}
+        maxRows={10}
+        {...register('body')}
+      />
+      {errors.body && <p>{errors.body?.message}</p>}
+
+      <Button variant="contained" type="submit" color="primary">
+        Send us your request
+      </Button>
+      {submitStatus ? (
+        <div>
+          <Snackbar
+            open={submitStatus}
+            autoHideDuration={2000}
+            message="Welcome!"
+          />
+        </div>
+      ) : (
+        ''
+      )}
+    </form>
+  );
 }
 
 export default ContactPage;
